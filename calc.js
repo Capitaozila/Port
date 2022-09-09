6060ccd32f257f47c10e0d48bed6cecc67bd7b64
Luis Brasil <48132029+Capitaozila@users.noreply.github.com>
2022-09-09 13:59:13 -0300

    calculadora

--- calc.js
+++ calc.js
@@ -2,21 +2,21 @@ let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
 
-buttons.map( button => {
+buttons.map(button => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
+        switch (e.target.innerText) {
             case 'C':
                 display.innerText = '';
                 break;
             case '=':
-                try{
+                try {
                     display.innerText = eval(display.innerText);
                 } catch {
                     display.innerText = "Erro"
                 }
                 break;
             case '←':
-                if (display.innerText){
+                if (display.innerText) {
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
