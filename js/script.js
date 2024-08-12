15732d9846dabc88f3ec5ba1d44e5f40ad74cc06
Capitaozila <luisdevtoo@gmail.com>
2024-08-12 18:01:33 -0300

    refactor: Update scroll behavior and add new typed strings in script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
-    new Typed(".typing", {
-        strings: ["Designer", "Programador", "Game Maker", "Web developer"], typeSpeed: 80, backSpeed: 30, loop: true
-    });
-    new Typed(".typing-2", {
-        strings: ["iniciante", "Front-end", "de jogos"], typeSpeed: 80, backSpeed: 30, loop: true
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  new Typed(".typing", {
+    strings: [
+      "Desenvolvedor Flutter",
+      "Desenvolvedor Python",
+      "Web developer",
+      "Desenvolvedor React",
+    ],
+    typeSpeed: 20,
+    backSpeed: 10,
+    loop: true,
+  });
+//   new Typed(".typing-2", {
+//     strings: ["Flutter", "Front-end", "de jogos"],
+//     typeSpeed: 20,
+//     backSpeed: 10,
+//     loop: true,
+//   });
 });
